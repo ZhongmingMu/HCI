d7459070b170d5042d2d538edcd86afaf0272182
zhongming <Zhongmingmu2015@u.northwestern.edu>
2016-02-29 23:07:28 -0600

    ..

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function jump()
+{	
+	window.location.href="../html/search.html";
+}
+
+function sign_in()
+{
+	window.location.href="../html/sign_in.html";
+}
+
+
+function sign_up()
+{
+	window.location.href="../html/sign_up.html";
+}
+
